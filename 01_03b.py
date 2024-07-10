e6aaa1c4d34a478394e715f3932777bf992bda5d
Raven Wei <125535543+weiraven@users.noreply.github.com>
2024-07-10 17:18:00 +0000

    uploading solve for third counter exercise

--- 01_03b.py
+++ 01_03b.py
@@ -1,8 +1,18 @@
+from collections import Counter
+
 def main():
     #Add code here
     inventory = Counter(STA001=10, SAL002=20, ENT004=13)
+    print(inventory)
+
     #sell 5 starters, 3 salads, and 3 entrees
+    remove_counts = Counter(STA001=5, SAL002=3, ENT004=3)
+    inventory -= remove_counts 
+    print(inventory)
+
     #make 9 more starters and 1 more entree
+    add_counts = Counter(STA001=9, ENT004=1)
+    inventory += add_counts
     print(inventory)
 
 if __name__ == "__main__":
